3de61d37146c3f4d6e7a690b73536b4e79053fff
Lior Agnin <lior.agnin@gmail.com>
2020-05-03 15:46:50 +0300

    fixed andriod small screen bug

--- lib/screens/splash/create_wallet.dart
+++ lib/screens/splash/create_wallet.dart
@@ -32,76 +32,89 @@ class _CreateWalletState extends State<CreateWallet> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset(
-                'assets/images/seedbed_logo.png',
-                width: 350,
-                height: 550,
+              Expanded(
+                flex: 6,
+                child: Image.asset(
+                  'assets/images/seedbed_logo.png',
+                  width: 350,
+                  height: 550,
+                ),
               ),
-              PrimaryButton(
-                fontSize: 16,
-                labelFontWeight: FontWeight.normal,
-                label: viewModel.isLoggedOut
-                    ? I18n.of(context).login
-                    : I18n.of(context).create_new_wallet,
-                onPressed: () async {
-                  if (viewModel.isLoggedOut) {
-                    viewModel.loginAgain();
-                  } else {
-                    viewModel.setDeviceIdCall();
-                    viewModel.createLocalAccount(() {
-                      setState(() {
-                        isPrimaryPreloading = false;
-                      });
-                      Router.navigator.pushNamed(Router.signupScreen);
-                    });
-                    setState(() {
-                      isPrimaryPreloading = true;
-                    });
-                  }
-                },
-                preload: isPrimaryPreloading,
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: viewModel.isLoggedOut
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            TransparentButton(
-                                fontSize: 14,
-                                label: I18n.of(context).restore_backup,
+              Flexible(
+                flex: 2,
+                child: Column(
+                  children: <Widget>[
+                    PrimaryButton(
+                      fontSize: 16,
+                      labelFontWeight: FontWeight.normal,
+                      label: viewModel.isLoggedOut
+                          ? I18n.of(context).login
+                          : I18n.of(context).create_new_wallet,
+                      onPressed: () async {
+                        if (viewModel.isLoggedOut) {
+                          viewModel.loginAgain();
+                        } else {
+                          viewModel.setDeviceIdCall();
+                          viewModel.createLocalAccount(() {
+                            setState(() {
+                              isPrimaryPreloading = false;
+                            });
+                            Router.navigator.pushNamed(Router.signupScreen);
+                          });
+                          setState(() {
+                            isPrimaryPreloading = true;
+                          });
+                        }
+                      },
+                      preload: isPrimaryPreloading,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: viewModel.isLoggedOut
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  TransparentButton(
+                                      fontSize: 14,
+                                      label: I18n.of(context).restore_backup,
+                                      onPressed: () async {
+                                        Router.navigator
+                                            .pushNamed(Router.recoveryPage);
+                                      }),
+                                  Text(
+                                    I18n.of(context).or,
+                                    style: TextStyle(color: Colors.grey[400]),
+                                  ),
+                                  TransparentButton(
+                                      fontSize: 14,
+                                      label: I18n.of(context).create__wallet,
+                                      onPressed: () async {
+                                        viewModel.setDeviceIdCall();
+                                        viewModel.createLocalAccount(() {
+                                          setState(() {
+                                            isTransparentPreloading = false;
+                                          });
+                                          Router.navigator
+                                              .pushNamed(Router.signupScreen);
+                                        });
+                                        setState(() {
+                                          isTransparentPreloading = true;
+                                        });
+                                      },
+                                      preload: isTransparentPreloading)
+                                ],
+                              )
+                            : TransparentButton(
+                                fontSize: 16,
+                                label: I18n.of(context).restore_from_backup,
                                 onPressed: () async {
-                                  Router.navigator.pushNamed(Router.recoveryPage);
-                                }),
-                            Text(
-                              I18n.of(context).or,
-                              style: TextStyle(color: Colors.grey[400]),
-                            ),
-                            TransparentButton(
-                                fontSize: 14,
-                                label: I18n.of(context).create__wallet,
-                                onPressed: () async {
-                                  viewModel.setDeviceIdCall();
-                                  viewModel.createLocalAccount(() {
-                                    setState(() {
-                                      isTransparentPreloading = false;
-                                    });
-                                    Router.navigator.pushNamed(Router.signupScreen);
-                                  });
-                                  setState(() {
-                                    isTransparentPreloading = true;
-                                  });
-                                },
-                                preload: isTransparentPreloading)
-                          ],
-                        )
-                      : TransparentButton(
-                          fontSize: 16,
-                          label: I18n.of(context).restore_from_backup,
-                          onPressed: () async {
-                            Router.navigator.pushNamed(Router.recoveryPage);
-                          }))
+                                  Router.navigator
+                                      .pushNamed(Router.recoveryPage);
+                                }))
+                  ],
+                ),
+              )
             ],
           );
         });
