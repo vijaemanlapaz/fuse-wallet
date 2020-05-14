d331d198473f7abc17d4d85a4f2ec0784a48d1d7
Tal Beja <tal@fuse.io>
2020-05-14 15:21:22 +0300

    pn fix

--- lib/utils/phone.dart
+++ lib/utils/phone.dart
@@ -1,11 +1,12 @@
 String formatPhoneNumber (String phoneNumber, String myCountryCode) {
   String countryCodeNumeric = myCountryCode.replaceFirst('+', '');
+  phoneNumber = (phoneNumber.length > 10 && phoneNumber.startsWith('0')) ? phoneNumber.replaceFirst('0', '+') : phoneNumber;
   phoneNumber = phoneNumber.replaceFirst(myCountryCode, '');
   phoneNumber = phoneNumber.startsWith(countryCodeNumeric) ? phoneNumber.replaceFirst(countryCodeNumeric, '') : phoneNumber;
   phoneNumber = phoneNumber.replaceAll(new RegExp('(-| |\\(0\\)|\\(0|\\(|\\))'), '');
   phoneNumber = phoneNumber.replaceFirst(new RegExp('^0+'), '');
   if (!phoneNumber.startsWith('+')) {
-    phoneNumber = myCountryCode + phoneNumber;
+    phoneNumber = (phoneNumber.length > 10 ? '+' : myCountryCode) + phoneNumber;
   }
   return phoneNumber;
 }
