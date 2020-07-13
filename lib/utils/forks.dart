039d98c40e956308fa2317ca749570324545d12c
Lior Agnin <lior.agnin@gmail.com>
2020-07-13 19:55:23 +0300

    minor fix

--- lib/utils/forks.dart
+++ lib/utils/forks.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String seedbBedCommuntiyAddress =
-    '0x104bac6fd3778944b733f128644e9aeca8f0dfbc';
-
 bool isFork() {
   return DotEnv().env['FORK'] != null && DotEnv().env['FORK'] == 'true';
 }
 
 bool isSeedBed(String communityAddress) {
   return communityAddress != null &&
-      communityAddress == seedbBedCommuntiyAddress;
+      communityAddress ==
+          DotEnv().env['DEFAULT_COMMUNITY_CONTRACT_ADDRESS'].toLowerCase();
 }
